2f33fe1be83c985b85e414fdcd7aa9b502b37ed7
sbx_user1051 <sbx_user1051@169.254.6.255>
2025-03-22 15:20:03 +0000

    Sat Mar 22 15:20:03 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,11 +7,26 @@ def checkout(skus):
     for c in skus:
         basket[c] = 1 + basket.get(c, 0)
     
+    if 'E' in basket and 'B' in basket:
+        if basket['E'] >= 2:
+            basket['B'] -= basket['E'] // 2
+            basket['B'] = max(0, basket['B'])
+
     total = 0
     print(basket)
+    Our price table and offers:
++------+-------+------------------------+
+| Item | Price | Special offers         |
++------+-------+------------------------+
+| A    | 50    | 3A for 130, 5A for 200 |
+| B    | 30    | 2B for 45              |
+| C    | 20    |                        |
+| D    | 15    |                        |
+| E    | 40    | 2E get one B free      |
++------+-------+------------------------+
     for item in basket:
         if item == 'A':
-            total += 130 * (basket[item] // 3) + 50 * (basket[item] % 3)
+            
         elif item == 'B':
             total += 45 * (basket[item] // 2) + 30 * (basket[item] % 2)
         elif item == 'C':
@@ -29,3 +44,4 @@ print(checkout('AABCD')) # 165
 print(checkout("AAABB"))
 print(checkout("CDBA"))
 print(checkout("AXA"))
+
