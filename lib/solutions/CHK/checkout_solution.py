f83430d15097a153b42b90ab1b574a6923b69ff6
sbx_user1051 <sbx_user1051@169.254.6.255>
2025-03-22 15:05:04 +0000

    Sat Mar 22 15:05:04 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,10 @@
-
+# from collections import defaultdict
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    print(skus)
+    basket = {}
+    for c in skus:
+        basket[c] = 1 + basket.get(basket[c] + 0)
+    
+    
