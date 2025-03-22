ac88b17fc6bffaa0bbd199d529852286037fccfa
sbx_user1051 <sbx_user1051@169.254.6.255>
2025-03-22 15:14:01 +0000

    Sat Mar 22 15:14:01 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,15 +7,13 @@ def checkout(skus):
     for c in skus:
         basket[c] = 1 + basket.get(c, 0)
     
-    # {'A': 2, 'B': 2, 'C': 1}
-
     total = 0
     print(basket)
     for item in basket:
         if item == 'A':
-            total += 50 * (basket[item] // 3) + 130 * (basket[item] % 3)
+            total += 130 * (basket[item] // 3) + 50 * (basket[item] % 3)
         elif item == 'B':
-            total += 30 * (basket[item] // 2) + 45 * (basket[item] % 2)
+            total += 45 * (basket[item] // 2) + 30 * (basket[item] % 2)
         elif item == 'C':
             total += 20 * basket[item]
         elif item == 'D':
@@ -26,6 +24,6 @@ def checkout(skus):
     return total
 
 print(checkout('AABCD')) # 165
-    
-
-
+print(checkout("AAABB"))
+print(checkout("CDBA"))
+print(checkout("AXA"))
